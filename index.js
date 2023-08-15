baf71a6043f4199c919b4ab56c73c4807d19f25e
vanessapigwin <72374043+vanessapigwin@users.noreply.github.com>
2023-08-15 16:25:25 +0800

    add prevent default

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const confirmArea = document.querySelector('#confirm-password');
 const inputRow = document.querySelector('.form-row:has(input#confirm-password)');
 const submitButton = document.querySelector('button');
 
-function validatePassword () {
+function validatePassword (e) {
+    e.preventDefault();
     if (passwordArea.value !== confirmArea.value) {
         alert('Passwords do not match, please try again.');
     }
+    else
+        window.location.href = "https://www.youtube.com/watch?v=hvL1339luv0";
+    
 }
 submitButton.addEventListener('click', validatePassword);
